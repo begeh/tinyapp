13f0e219f56b3527f05c1c3177cce8d40cb8d9b3
Bashir Egeh <b_egeh@hotmail.com>
2020-01-20 22:52:22 +0000

    created redirect for /u and added error message if shortURL doesn't exist

--- express_server.js
+++ express_server.js
@@ -3,25 +3,29 @@ const app = express();
 const PORT = 8080; //default port 8080
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  'bzxVn2': 'http://www.lighthouse.ca',
+  'bzxVn2': 'http://www.lighthouselabs.ca',
   '9sm5xk': 'http://www.google.com'
 };
 
-function generateRandomString(){
+function generateRandomString() {
   return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
 }
 
-app.post('/urls',(req,res) =>{
+app.post('/urls', (req, res) => {
   console.log(req.body);
-  res.send('OK');
+  let short = generateRandomString();
+  urlDatabase[short] = req.body.longURL;
+  let newURL = `/urls/${short}`;
+  res.redirect(newURL);
+  // res.send('OK');
 });
 
-app.get('/urls/new',(req, res) =>{
+app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
@@ -32,7 +36,15 @@ app.get('/urls', (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  if (templateVars.longURL === undefined) {
+    res.send("ERROR: URL NOT IN OUR DATABASE");
+  }
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.redirect(templateVars.longURL);
 });
 
 app.get('/', (req, res) => {
